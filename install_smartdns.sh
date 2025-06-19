ac55695c658def7a530efd599d4b940f1a2789ce
Meokj <153259973+Meokj@users.noreply.github.com>
2025-06-19 23:16:27 +0800

    Update install_smartdns.sh

--- install_smartdns.sh
+++ install_smartdns.sh
@@ -38,6 +38,9 @@ prefetch-domain yes
 server 8.8.8.8
 server 1.1.1.1
 server 223.5.5.5
+
+log-level info # 关闭日志级别设置为none，并注释下一行   
+log-file /var/log/smartdns.log
 EOF
 
 echo "[+] 重启并开机自启 SmartDNS..."
